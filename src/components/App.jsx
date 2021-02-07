8366ecddfe02ca4836129ae4edf37fbc495e744b
Ben Harrington <ben@harringtonben.com>
2021-02-07 10:40:11 -0600

    First commit

---
+++ src/components/App.jsx
@@ -0,0 +1,34 @@
+import React, { useState, useEffect } from 'react';
+import Hero from './Hero/Hero';
+import About from './About/About';
+import Contact from './Contact/Contact';
+import Footer from './Footer/Footer';
+
+import { PortfolioProvider } from '../context/context';
+
+import { heroData, aboutData, projectsData, contactData, footerData } from '../mock/data';
+
+function App() {
+  const [hero, setHero] = useState({});
+  const [about, setAbout] = useState({});
+  const [contact, setContact] = useState({});
+  const [footer, setFooter] = useState({});
+
+  useEffect(() => {
+    setHero({ ...heroData });
+    setAbout({ ...aboutData });
+    setContact({ ...contactData });
+    setFooter({ ...footerData });
+  }, []);
+
+  return (
+    <PortfolioProvider value={{ hero, about, contact, footer }}>
+      <Hero />
+      <About />
+      <Contact />
+      <Footer />
+    </PortfolioProvider>
+  );
+}
+
+export default App;
---
+++ src/mock/data.js
@@ -0,0 +1,59 @@
+import { nanoid } from 'nanoid';
+
+// HEAD DATA
+export const headData = {
+  title: 'Ben Harrington', // e.g: 'Name | Developer'
+  lang: 'en', // e.g: en, es, fr, jp
+  description: 'Welcome to my website', // e.g: Welcome to my website
+};
+
+// HERO DATA
+export const heroData = {
+  title: '',
+  name: 'Ben Harrington',
+  subtitle: 'I am a crocs enthusiast...and a software engineer',
+  cta: 'Get to know me',
+};
+
+// ABOUT DATA
+export const aboutData = {
+  img: 'headshot.jpg',
+  paragraphOne: 'Hi! I am a software engineer currently living in Nashville, TN. I love python, data, and integrating software. I currently work at Built Technologies, where I work on writing integrations and services for our various products.',
+  paragraphTwo: 'Whenever I am not working, I love cooking, hanging out with my wife and dog, and occasionally picking up a guitar and playing some music.',
+  paragraphThree: 'And of course...Crocs. They are hands down the most comfortable shoe out there and they have a pair for any occasion.',
+  resume: '', // if no resume, the button will not show up
+};
+
+
+// CONTACT DATA
+export const contactData = {
+  cta: 'Wanna talk crocs?',
+  btn: 'uh, duh',
+  email: 'ben@harringtonben.com',
+};
+
+// FOOTER DATA
+export const footerData = {
+  networks: [
+    {
+      id: nanoid(),
+      name: 'twitter',
+      url: 'https://twitter.com/benfromnormal',
+    },
+    {
+      id: nanoid(),
+      name: 'linkedin',
+      url: 'https://www.linkedin.com/in/ben-harrington-813b123b/',
+    },
+    {
+      id: nanoid(),
+      name: 'github',
+      url: 'https://github.com/harringtonben',
+    },
+  ],
+};
+
+// Github start/fork buttons
+export const githubButtons = {
+  isEnabled: false, // set to false to disable the GitHub stars/fork buttons
+};
